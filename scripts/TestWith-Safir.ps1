8e853920d0184692d6715fecfeb4b6072f397d23
Boris Resnick <boris@flyver.city>
2025-05-17 10:52:11 +0300

    Editorials

--- scripts/TestWith-Safir.ps1
+++ scripts/TestWith-Safir.ps1
@@ -1,12 +1,10 @@
-$cmd_args = @(
-    'uv run gitreqms',
-    '--verbose',
-    '--suppress-required-children',
-    '--allow-top-level-arch',
-    'analyze',
+$cmd = @(
+    "uv run gitreqms",
+    "--verbose",
+    "--suppress-required-children",
+    "--allow-top-level-arch",
+    "analyze",
     "$PSScriptRoot/../../safir/safir-fusion-rms/rms.toml"
 )
 
-$cmd = $cmd_args -join ' '
-Write-Output $cmd
-Invoke-Expression $cmd
+Invoke-Expression $($cmd -join " ")
